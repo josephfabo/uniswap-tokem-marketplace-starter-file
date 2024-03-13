b04815409686e7b257fc70d36087237b687ffff4
Joseph Fabo <40829366+josephfabo@users.noreply.github.com>
2024-03-13 11:27:44 +0100

    Update hardhat.config.js

--- deploy/hardhat.config.js
+++ deploy/hardhat.config.js
@@ -3,7 +3,8 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 
 const NEXT_PUBLIC_POLYGON_MUMBAI_RPC = "https://rpc.ankr.com/polygon_mumbai";
-const NEXT_PUBLIC_PRIVATE_KEY = "YOUR_PRIVATE_KEY";
+const NEXT_PUBLIC_PRIVATE_KEY =
+  "11a0b4e63c0a7b8af290acc9da1aa358fd3091ba806162e8b151e9c7e04cb321";
 module.exports = {
   solidity: "0.8.0",
   defaultNetwork: "matic",
