4e9a8f2fd0484621350306912fa0e4e3700ace48
Joseph Fabo <40829366+josephfabo@users.noreply.github.com>
2024-03-13 11:20:36 +0100

    Update contract.js

---
+++ utils/contract.js
@@ -0,0 +1,37 @@
+import { ethers } from "ethers";
+import CustomDexABI from "../utils/CustomDex.json";
+import CustomTokenABI from "../utils/CustomToken.json";
+
+export const tokenContract = async (address) => {
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const { ethereum } = window;
+
+  if (ethereum) {
+    const signer = provider.getSigner();
+
+    const contractReader = new ethers.Contract(
+      address,
+      CustomTokenABI.abi,
+      signer
+    );
+
+    return contractReader;
+  }
+};
+
+export const contract = async () => {
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const { ethereum } = window;
+
+  if (ethereum) {
+    const signer = provider.getSigner();
+
+    const contractReader = new ethers.contract(
+      "0xB14B50ca3BC2D4f4e2644ADcB87c49eDF4cF933a",
+      CustomDexABI,
+      signer
+    );
+
+    return contractReader;
+  }
+};
